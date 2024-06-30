a3dc6b2ea4f563cb6f424bdb4b7ba9bac0d9f71b
krishmvaidya <vaidya.krish@gmail.com>
2024-06-30 12:50:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score));
+//if we put "" then it becomes a string
+
+//if we wish to still use it as a number then we do the following
+
+let valueInNumber = Number(score)
+
+//this converts the string named 33 to a number 
+let isLoggedIn = 1
+
+let haha = Boolean (isLoggedIn)
+console.log(haha);
+
+// here we are converting number to boolean values
+// true-1, false-0, empty strig- false, string-true
